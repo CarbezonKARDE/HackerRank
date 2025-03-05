b1686bcd6d67536dd407975a55ce39a07ac269be
Aikansh karde <157959883+CarbezonKARDE@users.noreply.github.com>
2025-03-05 17:55:58 +0530

    Create Solve Me First.py

---
+++ Solve Me First.py
@@ -0,0 +1,9 @@
+def solveMeFirst(a, b):
+    return a + b
+def main():
+    num1 = int(input())
+    num2 = int(input())    
+    sum_result = solveMeFirst(num1, num2)    
+    print(sum_result)
+if __name__ == "__main__":
+    main()
